6d6e63715fbca6ed39f390d59a84707a673f9961
Sanjaya Kumar Nayak <sanjayanayak67@gmail.com>
2025-01-04 12:20:56 +0530

    checkEqual Arry and call,apply and bind

--- Javascript/javascriptInterviews/100_JS_coding_challanges/checkEqualArr.js
+++ Javascript/javascriptInterviews/100_JS_coding_challanges/checkEqualArr.js
@@ -12,3 +12,18 @@ let checkEqualArr = (input1, input2)=>{
 }
 
 console.log(checkEqualArr(arr,arr1));
+
+const checkEqualArr1 = (input1, input2)=>{
+    if(input1.length !== input2.length){
+        return false;
+    }
+    for(let i= 0; i<= input1.length; i++){
+        if(input1[i]===input2[i]){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+console.log(checkEqualArr1(arr, arr1));
